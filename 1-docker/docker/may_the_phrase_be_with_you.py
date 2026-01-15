a295ad0a5ecd1d8c777a7aeeb3f4fb1386b4e210
david-wilson-175 <35510393+david-wilson-175@users.noreply.github.com>
2026-01-14 22:26:27 -0700

    Initial commit

---
+++ 1-docker/docker/may_the_phrase_be_with_you.py
@@ -0,0 +1,32 @@
+import random
+import pyfiglet
+
+
+PHRASES = (
+    "It's a trap!",
+    "Stay on target.",
+    "That's no moon.",
+    "I have spoken.",
+    "Let the wookiee win.",
+    "Dagobah.",
+    "Let go, Luke!",
+    "I know.",
+    "My kind of scum.",
+    "Sorry about the mess.",
+    "All of it.",
+    "You'll be dead!",
+    "Oh, the uniform.",
+    "TK421",
+    "Punch it!",
+    "There is another",
+    "I don't like sand.",
+    "He's holding me back!"
+)
+
+def star_wars_quote():
+    phrase = random.choice(PHRASES)
+    ascii_art = pyfiglet.Figlet()
+    print(ascii_art.renderText(phrase))
+
+if __name__ == "__main__":
+    star_wars_quote()
---
+++ README.md
@@ -0,0 +1,5 @@
+# In-Class Exercises for IS 566
+Welcome to IS 566! This (public) repository is where I'll share various starter files and other code that we use in class. You'll probably want to clone this repository so that it lives alongside your own assignment repository folders. 
+
+> [!IMPORTANT] 
+> Most students like to enter code along with me as we go through things in class. If you do this, I recommend saving a copy of your local file within the `my_work` subfolder. Anything in this subfolder will be ignored by git, so this will help you avoid annoying merge conflicts that can happen if I make any changes to files you've already pulled down.
